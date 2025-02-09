dfac31057b28cde7b8b57feef969bc69451d7ebd
V. Rupendra <rupendra.v1069@gmail.com>
2025-02-09 12:10:29 +0530

    Add project files.

---
+++ PGconnectApi/Controllers/TestingController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PGconnectApi.Data;
+using PGconnectApi.Models;
+
+namespace PGconnectApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestingController : ControllerBase
+    {
+        private readonly PgConnectDbContext dbContext;
+
+        public TestingController(
+            PgConnectDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetTestingDetails()
+        {
+            var Testing =  dbContext.Testings.ToList();
+            return Ok(Testing);
+        }
+    }
+}
---
+++ PGconnectApi/Data/PgConnectDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PGconnectApi.Models;
+
+namespace PGconnectApi.Data
+{
+    public class PgConnectDbContext : DbContext
+    {
+        public PgConnectDbContext(DbContextOptions<PgConnectDbContext> PgConnectDbContext) : base(PgConnectDbContext)
+        {
+
+        }
+
+        public DbSet<Testing> Testings { get; set; }
+    }
+}
